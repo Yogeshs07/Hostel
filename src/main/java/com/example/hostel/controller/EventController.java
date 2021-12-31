36e5eb1fc3e7fd6753f082f159bd6289cfc6bb9b
jiayue <jiayue1030@gmail.com>
2021-12-31 10:19:30 +0800

    Add activePage attribute to EventController

--- src/main/java/com/example/hostel/controller/EventController.java
+++ src/main/java/com/example/hostel/controller/EventController.java
@@ -6,8 +6,10 @@ import com.example.hostel.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.text.ParseException;
 import java.util.List;
 
@@ -21,51 +23,63 @@ public class EventController {
     public String navigateToEventPage(Model model) {
         List<EventModel> eventList = eventService.getAllEvents();
         model.addAttribute("eventList", eventList);
-        model.addAttribute("activePage", "event");
+        model.addAttribute("activePage", "student");
         return "event";
     }
 
     @GetMapping("/event/new")
     public String navigateToNewEventFormPage(Model model) {
+        model.addAttribute("eventForm", new EventForm());
         model.addAttribute("event", null);
-        model.addAttribute("activePage", "event");
+        model.addAttribute("activePage", "student");
         return "event-form";
     }
 
     @GetMapping("/event/update/{eventId}")
     public String navigateToUpdateEventFormPage(@PathVariable Integer eventId, Model model) {
         EventModel event = eventService.getEvent(eventId);
-        model.addAttribute("activePage", "event");
+        model.addAttribute("eventForm", new EventForm());
         model.addAttribute("event", event);
+        model.addAttribute("activePage", "student");
         return "event-form";
     }
 
     @GetMapping("event/{eventId}")
     public String navigateToViewEventDetailsPage(@PathVariable Integer eventId, Model model) {
         EventModel event = eventService.getEvent(eventId);
-        model.addAttribute("activePage", "event");
         model.addAttribute("event", event);
+        model.addAttribute("activePage", "student");
         return "event-detail";
     }
 
     @PostMapping("/create-event")
-    public String createNewEvent(@ModelAttribute EventForm eventForm, Model model) throws ParseException {
+    public String createNewEvent(@Valid @ModelAttribute EventForm eventForm, BindingResult bindingResult, Model model) throws ParseException {
+        boolean formHasError = bindingResult.hasErrors();
+        if (formHasError) {
+            model.addAttribute("eventForm", eventForm);
+            return "event-form";
+        }
         eventService.createNewEvent(eventForm);
-        model.addAttribute("activePage", "event");
+        model.addAttribute("activePage", "student");
         return "redirect:/event";
     }
 
     @PostMapping("/update-event")
-    public String updateEvent(@ModelAttribute EventForm eventForm, Model model) throws ParseException {
+    public String updateEvent(@Valid @ModelAttribute EventForm eventForm, BindingResult bindingResult, Model model) throws ParseException {
+        boolean formHasError = bindingResult.hasErrors();
+        if (formHasError) {
+            model.addAttribute("eventForm", eventForm);
+            return "event-form";
+        }
         eventService.updateEvent(eventForm);
-        model.addAttribute("activePage", "event");
+        model.addAttribute("activePage", "student");
         return "redirect:/event/" + eventForm.getEventId().toString();
     }
 
     @GetMapping("/delete-event/{eventId}")
     public String deleteEvent(@PathVariable Integer eventId, Model model) {
         eventService.deleteEvent(eventId);
-        model.addAttribute("activePage", "event");
+        model.addAttribute("activePage", "student");
         return "redirect:/event";
     }
 
